e0bb2875a5d4dd32f482147f79f86d92c6b1b5f9
fabiaaaAkter <98603208+fabiaaaAkter@users.noreply.github.com>
2022-12-07 01:32:50 +0600

    Add files via upload

---
+++ background colour.c
@@ -0,0 +1,6 @@
+void SetColorAndBackground ( int ForgC , int BackC )
+{
+     WORD wColor = ( ( BackC & 0x0F ) < < 4 ) + ( ForgC & 0x0F ) ;
+     SetConsoleTextAttribute ( GetStdHandle ( STD_OUTPUT_HANDLE ) , wColor ) ;
+     return ;
+}
