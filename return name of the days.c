da0dac8f29d89ceb68047eb38731f157245d3157
fabiaaaAkter <98603208+fabiaaaAkter@users.noreply.github.com>
2022-12-07 01:47:07 +0600

    Add files via upload

---
+++ return name of the days.c
@@ -0,0 +1,12 @@
+char * getName ( int day ) { //returns the name of the day
+   switch ( day ) {
+      case 0 : return ( " Sunday " ) ;
+      case 1 : return ( " Monday " ) ;
+      case 2 : return ( " Tuesday " ) ;
+      case 3 : return ( " Wednesday " ) ;
+      case 4 : return ( " Thursday " ) ;
+      case 5 : return ( " Friday " ) ;
+      case 6 : return ( " Saturday " ) ;
+      default : return ( " Error in getName ( ) module.Invalid argument passed " ) ;
+   }
+}
